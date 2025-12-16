99872df697ec0ba4a2ce24506f3a4dd1bfa68422
munlee <mathvs007@gmail.com>
2025-12-16 17:00:05 +0300

    лаба6

---
+++ lab6.cpp
@@ -0,0 +1,206 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+// Константы для оформления таблицы
+const int numberOfTableColumns = 4;
+const int firstColumnWidth = 20;
+const int secondColumnWidth = 18;
+const int thirdColumnWidth = 18;
+const int fourthColumnWidth = 12;
+
+// Символы рамки в UTF-8
+const char* ul = "┌";
+const char* ur = "┐";
+const char* dl = "└";
+const char* dr = "┘";
+const char* vt = "│";
+const char* hz = "─";
+const char* cr = "┼";
+const char* Td = "┬";
+const char* Tu = "┴";
+const char* Tr = "├";
+const char* Tl = "┤";
+
+struct ResultToPrint {
+    char* name;      // название функции
+    double i_sum;    // численно рассчитанный интеграл
+    double i_toch;   // точное значение интеграла
+    int n;           // число разбиений
+};
+
+// Функция для печати горизонтальной линии таблицы
+void printHorizontalLine(int col_widths[], const char* left, const char* middle, const char* right) {
+    cout << left;
+    for (int j = 0; j < numberOfTableColumns; j++) {
+        for (int k = 0; k < col_widths[j] + 2; k++) {
+            cout << hz;
+        }
+        if (j < numberOfTableColumns - 1) {
+            cout << middle;
+        }
+    }
+    cout << right << endl;
+}
+
+// Функция вывода таблицы (исправленная версия)
+void printTabl(ResultToPrint* i_prn, int countRowOfTable) {
+    int widthOfTableColumns[numberOfTableColumns] = {
+        firstColumnWidth, secondColumnWidth,
+        thirdColumnWidth, fourthColumnWidth
+    };
+
+    char title[numberOfTableColumns][50] = {
+        "Function", "Integral", "IntSum", "N"
+    };
+
+    printHorizontalLine(widthOfTableColumns, ul, Td, ur);
+
+    cout << vt;
+    for (int j = 0; j < numberOfTableColumns; j++) {
+        int title_len = strlen(title[j]);
+        int left_spaces = (widthOfTableColumns[j] - title_len) / 2;
+        int right_spaces = widthOfTableColumns[j] - title_len - left_spaces;
+
+        cout << " ";
+        for (int k = 0; k < left_spaces; k++) cout << " ";
+        cout << title[j];
+        for (int k = 0; k < right_spaces; k++) cout << " ";
+        cout << " " << vt;
+    }
+    cout << endl;
+
+    printHorizontalLine(widthOfTableColumns, Tr, cr, Tl);
+
+    // Тело таблицы
+    for (int i = 0; i < countRowOfTable; i++) {
+        cout << vt;
+
+        cout << " " << setw(widthOfTableColumns[0]) << left << i_prn[i].name << " " << vt;
+
+        cout << " " << setw(widthOfTableColumns[1]) << right << fixed << setprecision(6)
+             << i_prn[i].i_toch << " " << vt;
+
+        cout << " " << setw(widthOfTableColumns[2]) << right << fixed << setprecision(6)
+             << i_prn[i].i_sum << " " << vt;
+
+        cout << " " << setw(widthOfTableColumns[3]) << right << i_prn[i].n << " " << vt;
+
+        cout << endl;
+
+        if (i < countRowOfTable - 1) {
+            printHorizontalLine(widthOfTableColumns, Tr, cr, Tl);
+        }
+    }
+
+    printHorizontalLine(widthOfTableColumns, dl, Tu, dr);
+    cout << endl;
+}
+
+// Тип указателя на функцию
+typedef double (*TPF)(double);
+
+// Функции для численного интегрирования
+double integrationByRectangle(TPF f, double a, double b, double eps, int& n) {
+    double I1, I2;
+    double h;
+    n = 1;
+    I2 = 0.0;
+
+    do {
+        I1 = I2;
+        n *= 2;
+        h = (b - a) / n;
+        I2 = 0.0;
+
+        // Метод средних прямоугольников
+        for (int i = 0; i < n; i++) {
+            double x = a + (i + 0.5) * h;
+            I2 += f(x);
+        }
+        I2 *= h;
+
+    } while (fabs(I2 - I1) / 3 >= eps);
+
+    return I2;
+}
+
+double integrationByTrapezoidal(TPF f, double a, double b, double eps, int& n) {
+    double I1, I2;
+    double h;
+    n = 1;
+    I2 = 0.0;
+
+    do {
+        I1 = I2;
+        n *= 2;
+        h = (b - a) / n;
+        I2 = (f(a) + f(b)) / 2.0;
+
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            I2 += f(x);
+        }
+        I2 *= h;
+
+    } while (fabs(I2 - I1) / 3 >= eps);
+
+    return I2;
+}
+
+// Подынтегральные функции
+double f1(double x) { return x; }
+double f2(double x) { return sin(22 * x); }
+double f3(double x) { return pow(x, 4); }
+double f4(double x) { return atan(x); }
+
+int main() {
+    const double a = 0.0;
+    const double b = 1.0;
+
+    TPF funcs[] = { f1, f2, f3, f4 };
+
+    const char* names[] = { "f1(x)=x", "f2(x)=sin(22x)", "f3(x)=x^4", "f4(x)=arctg(x)" };
+
+    double exact[] = {
+        0.5,
+        (1.0 - cos(22.0)) / 22.0,
+        0.2,
+        atan(1.0) - 0.5 * log(2.0)
+    };
+
+    double epsilons[] = { 0.01, 0.001, 0.0001, 0.00001, 0.000001 };
+    int numEps = 5;
+    int numFuncs = 4;
+
+    ResultToPrint* results = new ResultToPrint[numFuncs];
+
+    cout << "МЕТОД ПРЯМОУГОЛЬНИКОВ" << endl << endl;
+
+    for (int i = 0; i < numEps; i++) {
+        double eps = epsilons[i];
+        cout << "Точность eps = " << eps << endl;
+
+        for (int j = 0; j < numFuncs; j++) {
+            results[j].name = new char[strlen(names[j]) + 1];
+            strcpy(results[j].name, names[j]);
+
+            results[j].i_toch = exact[j];
+            int n = 0;
+            results[j].i_sum = integrationByRectangle(funcs[j], a, b, eps, n);
+            results[j].n = n;
+        }
+
+        printTabl(results, numFuncs);
+
+        for (int j = 0; j < numFuncs; j++) {
+            delete[] results[j].name;
+        }
+    }
+
+    delete[] results;
+    return 0;
+}
